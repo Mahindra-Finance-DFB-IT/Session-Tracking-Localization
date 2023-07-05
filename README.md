715f4e95a06663494ff8638f04573c1352df047f
Raviraj K <cordovaravi@gmail.com>
2023-07-05 16:21:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# globalization_ex
+
+Poc on flutter translation using ML APis in Flutter 
+
+## 
---
+++ android/app/src/main/kotlin/com/example/globalization_ex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.globalization_ex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
